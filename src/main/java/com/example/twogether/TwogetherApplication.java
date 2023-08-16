df6b6fee10c0dfe7581b38b4937dfb135c158971
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-16 16:41:21 +0900

    [init] 프로젝트 초기 환경 설정 (+ PR 템플릿, README)

---
+++ src/main/java/com/example/twogether/TwogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.twogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twogether/TwogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
