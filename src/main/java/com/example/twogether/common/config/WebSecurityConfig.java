e75a239d0ff938edfff47c8d988ada7e4d235274
Han JiHun <hjh_3229@naver.com>
2023-08-17 16:46:06 +0900

    [update] WebSecurityConfig에 /api/decks 추가

--- src/main/java/com/example/twogether/common/config/WebSecurityConfig.java
+++ src/main/java/com/example/twogether/common/config/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig {
             authorizeHttpRequests
                 .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                .requestMatchers("/api/decks").permitAll()
                 .anyRequest().authenticated()
         );
 
