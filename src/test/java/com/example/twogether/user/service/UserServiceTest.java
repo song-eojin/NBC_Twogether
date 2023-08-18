49c39b456df16d2725ce16a87d7811ed45d4234c
proLmpa <52267654+proLmpa@users.noreply.github.com>
2023-08-18 12:50:41 +0900

    [feature] 사용자 정보 수정/삭제 및 비밀번호 변경 기능 구현 후 테스트 코드 작성 (#20)
    * [complete] 사용자 정보 수정 및 회원 탈퇴 기능 구현
    
    * [complete] 사용자 정보 수정 및 회원 탈퇴 기능 테스트 코드 작성
    
    * [init] 사용자 비밀번호 관련 entity, dto, repository 생성
    
    * [complete] 사용자 비밀번호 수정 기능 구현 (+ controller swagger description 필드 제거)
    
    * [complete] 사용자 비밀번호 수정 기능 테스트 코드 작성
    
    * [update] 최근 사용된 비밀번호 조회 기능의 조회 비밀번호 수를 3회에서 2회로 줄인 후 기존에 사용되던 혹은 바로 직전에 사용했던 비밀번호 까지만 이력에 저장하도록 기능 수정
    
    * [update] 상황에 따라 적절한 HttpStatus 에러 상태 코드 반환하기
    
    * [update] HttpStatus 대신 class 별로 맞춤된 에러 코드 반환하기
    
    * [update] controller에 @RequiredArgsConstructor 사용(코드 컨벤션)

--- src/test/java/com/example/twogether/user/service/UserServiceTest.java
+++ src/test/java/com/example/twogether/user/service/UserServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.twogether.user.service;
 
+import com.example.twogether.common.error.CustomErrorCode;
+import com.example.twogether.common.exception.CustomException;
+import com.example.twogether.user.dto.EditPasswordRequestDto;
+import com.example.twogether.user.dto.EditUserRequestDto;
 import com.example.twogether.user.dto.SignupRequestDto;
 import com.example.twogether.user.entity.User;
 import com.example.twogether.user.entity.UserRoleEnum;
 import com.example.twogether.user.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -30,8 +35,7 @@ class UserServiceTest {
 
     private User user;
 
-    @Test
-    @DisplayName("회원 가입 테스트")
+    @BeforeEach
     void signUp() {
         // given
         String email = "user2024@email.com";
@@ -40,7 +44,8 @@ class UserServiceTest {
         String adminToken = "";
 
         encoder = new BCryptPasswordEncoder();
-        SignupRequestDto request = SignupRequestDto.builder().email(email).password(password).admin(admin).adminToken(adminToken).build();
+        SignupRequestDto request = SignupRequestDto.builder().email(email).password(password)
+            .admin(admin).adminToken(adminToken).build();
 
         // when
         User signed = userService.signup(request);
@@ -52,50 +57,101 @@ class UserServiceTest {
         user = signed;
     }
 
-//    @Test
-//    @DisplayName("사용자 정보 수정")
-//    void editUserInfo() {
-//        // given
-//        String nickname = "I'm Owl";
-//        String introduction = "This is my child owo";
-//
-//        EditUserRequestDto requestDto = new EditUserRequestDto(nickname, introduction);
-//
-//        // when
-//        User edited = userService.editUserInfo(requestDto, user);
-//
-//        // then
-//        Assertions.assertEquals(nickname, edited.getNickname());
-//        Assertions.assertEquals(introduction, edited.getIntroduction());
-//    }
-//
-//    @Test
-//    @DisplayName("사용자 정보 삭제")
-//    void deleteUserInfo() {
-//        // given
-//
-//        // when
-//        userService.deleteUserInfo(user.getId(), user);
-//
-//        // then
-//        Assertions.assertNull(userRepository.findById(user.getId()).orElse(null));
-//    }
-//
-//    @Test
-//    @DisplayName("사용자 비밀번호 수정")
-//    void editUserPassword() {
-//        // given
-//        String password = "user123!@#";
-//        String newPassword1 = "user234@#$";
-//        String newPassword2 = "user234@#$";
-//
-//        EditPasswordRequestDto correctdto = new EditPasswordRequestDto(password, newPassword1, newPassword2);
-//
-//        // when - 정상 비밀번호 수정
-//        User edited = userService.editUserPassword(correctdto, user);
-//
-//        // then
-//        Assertions.assertFalse(encoder.matches(password, edited.getPassword()));
-//        Assertions.assertTrue(encoder.matches(newPassword1, edited.getPassword()));
-//    }
+    @Test
+    @DisplayName("중복 회원 가입 실패")
+    void SignUpFailed() {
+        // given
+        String email = "user2024@email.com";
+        String password = "user123!@#";
+        boolean admin = false;
+        String adminToken = "";
+
+        encoder = new BCryptPasswordEncoder();
+        SignupRequestDto request = SignupRequestDto.builder().email(email).password(password)
+            .admin(admin).adminToken(adminToken).build();
+
+        // when - then
+        try {
+            userService.signup(request);
+        } catch (CustomException e) {
+            Assertions.assertEquals(CustomErrorCode.USER_ALREADY_EXISTS, e.getErrorCode());
+        }
+    }
+
+    @Test
+    @DisplayName("사용자 정보 수정")
+    void editUserInfo() {
+        // given
+        String nickname = "I'm Owl";
+        String introduction = "This is my child owo";
+
+        EditUserRequestDto requestDto = new EditUserRequestDto(nickname, introduction);
+
+        // when
+        User edited = userService.editUserInfo(requestDto, user);
+
+        // then
+        Assertions.assertEquals(nickname, edited.getNickname());
+        Assertions.assertEquals(introduction, edited.getIntroduction());
+    }
+
+    @Test
+    @DisplayName("사용자 정보 삭제")
+    void deleteUserInfo() {
+        // given
+        long userId = user.getId();
+
+        // when
+        userService.deleteUserInfo(userId, user);
+
+        // then
+        Assertions.assertNull(userRepository.findById(userId).orElse(null));
+    }
+
+    @Test
+    @DisplayName("사용자 비밀번호 수정")
+    void editUserPassword() {
+        // given
+        String password = "user123!@#";
+        String newPassword = "user234@#$";
+
+        EditPasswordRequestDto requestDto = EditPasswordRequestDto.builder().password(password)
+            .newPassword(newPassword).build();
+
+        // when - 정상 비밀번호 수정
+        User edited = userService.editUserPassword(requestDto, user);
+
+        // then
+        Assertions.assertFalse(encoder.matches(password, edited.getPassword()));
+        Assertions.assertTrue(encoder.matches(newPassword, edited.getPassword()));
+    }
+
+    @Test
+    @DisplayName("사용자 비밀번호 수정 실패")
+    void editUserPasswordFailed() {
+        // given
+        String password = "user123!@#";
+        String newPassword = "user234@#$";
+
+        EditPasswordRequestDto recentlyUsed = EditPasswordRequestDto.builder().password(password)
+            .newPassword(password).build();
+
+        EditPasswordRequestDto wrongPassword = EditPasswordRequestDto.builder()
+            .password(newPassword)
+            .newPassword(newPassword).build();
+
+        // when - then(1) : 최근 사용한 비밀번호 사용 불가
+        try {
+            userService.editUserPassword(recentlyUsed, user);
+        } catch (CustomException e) {
+            Assertions.assertEquals(CustomErrorCode.PASSWORD_RECENTLY_USED, e.getErrorCode());
+        }
+
+        // when - then(2) : 잘못된 비밀번호 입력
+        try {
+            userService.editUserPassword(wrongPassword, user);
+        } catch (CustomException e) {
+            Assertions.assertEquals(CustomErrorCode.PASSWORD_MISMATCHED, e.getErrorCode());
+        }
+    }
 }
